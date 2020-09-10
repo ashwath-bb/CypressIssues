23f3edbc357a04bf6fed687de5ae79a23e236b9e
AshwathB <ashwath.biradar@bigbinary.com>
2020-09-10 23:16:13 +0530

    Changed Login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,9 +3,26 @@
 class Api::V1::SessionsController < Devise::SessionsController
   skip_before_action :verify_authenticity_token
 
+  def create
+    user = User.find_for_database_authentication(email: params[:user] && params[:user][:email])
+
+    if invalid_password?(user)
+      flash[:notice] = "Incorrect email or password"
+    else
+      sign_in "user", user
+      flash[:notice] = 'Successfully logged in'
+    end
+    redirect_to root_path
+  end
+
   def destroy
     sign_out(current_user)
-    reset_session
     redirect_to destroy_user_session_path
   end
+
+  private
+
+    def invalid_password?(user)
+      user.blank? || !user.valid_password?(params[:user][:password])
+    end
 end
